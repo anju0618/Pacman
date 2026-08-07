a18654fbb88a4c002326fb59a7cd6ba84ab74fad
takawaka <user-mail-dayangchuanshang@gmail.com>
2026-08-07 14:48:36 +0900

    move content in __main__.py to pac-man.py

---
+++ src/pac-man.py
@@ -0,0 +1,19 @@
+import argparse
+from src.parse import Parsing
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="packman"
+    )
+    parser.add_argument("configfile", help="ConfigFile")
+    args = parser.parse_args()
+    print(args.configfile)
+    print("=" * 30)
+    file_parser = Parsing()
+    config = file_parser.parse_file(args.configfile)
+    print(config)
+
+
+if __name__ == "__main__":
+    main()
