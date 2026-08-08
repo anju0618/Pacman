afa6cbde3c60b0ce9675afc074eb0a847f40fea0
Anjou Makino <amakino@c6r3s1.42tokyo.jp>
2026-08-08 11:52:37 +0900

    map gen and pacman gen

--- src/character/pacman.py
+++ src/character/pacman.py
@@ -2,10 +2,20 @@
 pacman mod
 """
 from src.character.base import Character
+from src.enums import Direction
 
 
 class Pacman(Character):
 
-    def __init__(self, start_x: float, start_y: float) -> None:
-        super().__init__(start_x, start_y)
-        self.c_gum: int = 0
+    def __init__(self, x: float, y: float):
+        super().__init__(x, y)
+        self.direction = Direction.RIGHT
+
+    def set_direction(self, direction: Direction):
+        """プレイヤーからの入力方向を受け取る"""
+        self.direction = direction
+
+    def update(self, maze_data: list[list[int]]):
+        """毎フレーム呼ばれる更新処理"""
+        self.move_forward(maze_data)
+
